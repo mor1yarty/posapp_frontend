6019af9b70f334a2c1a1925beb4d35e97888f4bf
moriya-wsl <y.moriya.113@gmail.com>
2025-06-09 03:50:20 +0900

    next.config.jsにoutput: 'standalone'を追加

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   async rewrites() {
     const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000';
     return [
